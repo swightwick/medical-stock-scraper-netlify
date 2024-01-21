9702d4f93c13a0158405c70f4b1791dafbb3a730
Sam <info@samwightwick.co.uk>
2024-01-21 17:18:42 +0000

    package functions

--- netlify-functions/scrape.js
+++ netlify-functions/scrape.js
@@ -1,5 +1,4 @@
-const puppeteer = require('puppeteer');
-
+const puppeteer = require('puppeteer-core');
 exports.handler = async function (event, context) {
   let availableFlowers = [];
   let lastScrapeTimestamp = '';
