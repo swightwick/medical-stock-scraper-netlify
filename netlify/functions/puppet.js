2c0231de422356e8de0c274cbbf0c89b7e8e04ec
Sam <info@samwightwick.co.uk>
2024-01-28 22:10:15 +0000

    plase wrk

--- netlify/functions/puppet.js
+++ netlify/functions/puppet.js
@@ -3,7 +3,7 @@ import puppeteer from 'puppeteer-core'
 
 chromium.setHeadlessMode = true
 chromium.setGraphicsMode = false
-const url = 'https://lite.cnn.com/'
+const url = 'https://www.mamedica.co.uk/repeat-prescription/'
 
 export async function handler(event, context) {
 
@@ -17,26 +17,60 @@ export async function handler(event, context) {
       executablePath: process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath('/var/task/node_modules/@sparticuz/chromium/bin')),
     })
 
-    const page = await browser.newPage()
-
+    console.log('Opening Browser - pup script');
+    const page = await browser.newPage();
+    console.log('here 1');
     await page.goto(url)
+    console.log('here 2');
+    await page.waitForTimeout(100) 
+    await page.waitForSelector('#field_3_31')
+    console.log('here 3');
+    await page.click('#label_3_31_0');
+    await page.click('#label_3_45_0');
+    await page.click('#label_3_32_0');
+    console.log('here 4');
+    await page.waitForSelector('#field_3_50 .selectric-scroll')
+    console.log('here 5');
 
-    await page.waitForSelector('.title')
+    // capture all the items in stock list
+    let elements = await page.$$('#field_3_50 .selectric-scroll ul li');
 
-    const results = await page.$$eval('ul li', (articles) => {
-      return articles.map((link) => {
-        return {
-          title: link.querySelector('a').innerText,
-          url: link.querySelector('a').href,
-        }
+    // loop trough items
+    availableFlowers = [];
+    for (let i = 1; i < elements.length; i++) {
+      await page.click('#field_3_50 b.button');
+      await page.click(`#field_3_50 .selectric-items .selectric-scroll ul li[data-index*="${i}"]`);
+      const flower = await page.$eval(`#field_3_50 .selectric-items .selectric-scroll ul li[data-index*="${i}"]`, e => e.innerText);
+      await page.waitForTimeout(10) 
+      await page.$eval('#input_3_53', el => el.value = '1');
+      await page.click('#field_3_77');
+      await page.waitForTimeout(20) 
+      const getCosts = await page.$eval('#input_3_67', e => e.value);
+      const cost = getCosts.replace(/\s/g, '');
+      await page.waitForTimeout(20) 
+      availableFlowers.push({ "item": 
+        {"flower": flower, "cost": cost}
       })
-    })
+    }
 
+    // lastScrapeTimestamp = new Date().toLocaleString('en-GB', {
+    //   year: 'numeric',
+    //   month: 'numeric',
+    //   day: 'numeric',
+    //   hour: 'numeric',
+    //   minute: 'numeric',
+    //   second: 'numeric',
+    //   hour12: false,
+    // });
+    // console.log('Scraped data at', lastScrapeTimestamp);
     await browser.close()
+
 
     return {
       statusCode: 200,
-      body: JSON.stringify(results),
+      body: JSON.stringify({
+        availableFlowers
+      })
     }
   } catch (error) {
     console.error(error)
